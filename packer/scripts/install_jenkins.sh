5821997defacce252ca7d406e9e06395c5b267ce
phmean21 <descry2015@gmail.com>
2021-06-15 06:00:58 -0700

    tested packer

--- packer/scripts/install_jenkins.sh
+++ packer/scripts/install_jenkins.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-set -e
-sudo install openjdk-8-jre
+#set -e
+sudo apt-get -y update 
+sudo apt-get install -y openjdk-8-jre   
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-get update
-sudo apt-get install jenkins
+sudo apt-get -y update 
+sudo apt-get -y install jenkins 
---
+++ packer/variables.pkr.hcl
@@ -0,0 +1,4 @@
+variable "project" {
+  type = string
+  default = "week-3-314020"
+}
