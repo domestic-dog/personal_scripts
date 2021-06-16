7ed7ed76bd8d0353fa20fa25f5d8a54ea3d56516
phmean21 <descry2015@gmail.com>
2021-06-16 01:53:00 -0700

    rm comments

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -3,17 +3,10 @@ default = "week-3-314020"
 }
 variable zone {
 description = "zone"
-# Значение по умолчанию
 default = "europe-west1-b"
 }
 variable public_key_path {
 default = "./secrets/id_rsa.pub"
 description = "Path to the public key used for ssh access"
 }
-
-
-#variable app_disk_img {
-#description = "Disk image for reddit db"
-#default = "app-base-1621678709"
-#}
 
